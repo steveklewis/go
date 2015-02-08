cc2d811184a6d00ef002a09b131b2249e4301669
steveklewis <lewisksteven@gmail.com>
2015-02-07 23:21:26 -0500

    Documenting how to get meta key to work on Mac terminal.

--- emacs_config/init.el
+++ emacs_config/init.el
@@ -1,3 +1,7 @@
+; On a Mac, how to get the meta key to work:
+; http://stackoverflow.com/questions/162896/emacs-on-mac-os-x-leopard-key-bindin
+gs
+
 (add-to-list 'load-path "~/workspace/git/go/emacs_config/")
 (require 'go-mode-autoloads)
 
