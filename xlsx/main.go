1b31ac106651354936f9aba20c5166687623cbd7
steveklewis <lewisksteven@gmail.com>
2015-02-18 11:23:05 -0500

    Tried updating Excel file, seems to work.

--- xlsx/main.go
+++ xlsx/main.go
@@ -32,4 +32,19 @@ func main() {
 	if err != nil {
 		fmt.Printf(err.Error())
 	}
+
+	file.Sheets[0].Rows[0].Cells[0].Value = "I am a cell2!"
+
+	for _, tempSheet := range file.Sheets {
+		for _, tempRow := range tempSheet.Rows {
+			for _, tempCell := range tempRow.Cells {
+				fmt.Printf("%s\n", tempCell.String())
+			}
+		}
+	}
+
+	err = file.Save("SecondExcel.xlsx")
+	if err != nil {
+		fmt.Printf(err.Error())
+	}
 }
